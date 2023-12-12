a23797b53a7100e7cf1f18fd6df4943801c09f99
mohammadrezadehbashi <mrdt71@gmail.com>
2023-12-12 13:13:48 +0330

    madular

--- src/app/Footer/index.tsx
+++ src/app/Footer/index.tsx
@@ -10,74 +10,19 @@ import support from '/public/icons/support.svg';
 import cash from '/public/icons/cash-on-delivery.svg';
 import orginal from '/public/icons/original-products.svg';
 import Linker_footer from './components/Linker_footer';
+import TopFooter from './components/TopFooter';
+import BottomFooter from './components/BottomFooter';
 
 const Footer = () => {
     return (
         <Box sx={hideOnMobile} style={{ backgroundColor: "blue" }}>
             <Stack sx={{ bgcolor: "green", width: "100%" }}>
-                <Box sx={{ display: "flex", justifyContent: "space-between", flexGrow: 1, marginInline: 3, mt: 3 }} style={{ backgroundColor: "orange" }}>
-                    <Box >
-                        <Button variant="outlined" startIcon={<KeyboardArrowUpIcon />}>
-                            بازگشت به بالا
-                        </Button>
-                    </Box>
-                    <Box sx={{ mt: "-10px", justifyContent: "start", direction: "rtl" }}>
-                        <Image src={logo} alt='logo' width={120} height={50} ></Image>
-                        <Typography>تلفن پشتیبانی ۶۱۹۳۰۰۰۰ - ۰۲۱
-                            |
-                            ۷ روز هفته، ۲۴ ساعته پاسخگوی شما هستیم</Typography>
-                    </Box>
-                </Box>
 
-                <Box sx={{ bgcolor: "red", display: "flex",justifyContent:"space-around",marginBlock:'40px'}}>
-                    <Box>
-                        <Image src={delivery} alt='logo' width={120} height={50}></Image>
-                        <Typography>امکان تحویل اکسپرس</Typography>
-                    </Box>
-                    <Box>
-                        <Image src={daysReturn} alt='logo' width={120} height={50}></Image>
-                        <Typography>هفت روز ضمانت بازگشت کالا</Typography>
-                    </Box>
-                    <Box>
-                        <Image src={support} alt='logo' width={120} height={50}></Image>
-                        <Typography> هفت روز هفته و 24 ساعته </Typography>
-                    </Box>
-                    <Box>
-                        <Image src={cash} alt='logo' width={120} height={50}></Image>
-                        <Typography>امکان پرداخت در محل</Typography>
-                    </Box>
-                    <Box>
-                        <Image src={orginal} alt='logo' width={120} height={50}></Image>
-                        <Typography>ضمانت اصل بودن کالا</Typography>
-                    </Box>
-                </Box>
-
-                <Linker_footer/>
+                <TopFooter />
+                <Linker_footer />
 
 
-                <Box sx={{borderTop:".5px solid grey",marginInline:3}}>
-                    <Typography sx={{paddingBlock:"20px",textAlign:"center"}} variant="body2">برای استفاده از مطالب دیجی‌کالا، داشتن «هدف غیرتجاری» و ذکر «منبع» کافیست. تمام حقوق اين وب‌سايت نیز برای شرکت نوآوران فن آوازه (فروشگاه آنلاین دیجی‌کالا) است.</Typography>
-                </Box>
-                <Box sx={{display:"flex",background:"blue",marginInline:.5}}>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                </Box>
-                <Box sx={{display:"flex",background:"blue",marginInline:.5}}>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                    <Box sx={{border:"1.5px solid grey",textAlign:"center",paddingBlock:"25px",width:"calc(100% - 12px)"}}>aadda</Box>
-                </Box>
+                <BottomFooter/>
             </Stack>
         </Box>
     );
