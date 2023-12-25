a2f59ee24e5ec298bb77ddac65d3e0bb3946b81a
mohammadrezadehbashi <mrdt71@gmail.com>
2023-12-25 21:44:12 +0330

    auth and shop

---
+++ src/app/(shop)/shop-mr/product/layout.tsx
@@ -0,0 +1,17 @@
+// "use client";
+import Footer from "../Footer";
+import Header from "../Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
