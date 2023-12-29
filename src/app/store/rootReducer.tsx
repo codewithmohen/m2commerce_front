5802b6ce7babacec451fb7cf2999a01238b52743
Mohsen Karimian (Hajikarimian) <135535893+codewithmohsen@users.noreply.github.com>
2023-12-29 21:19:03 +0330

    dynamic reducer finder done

--- src/app/store/rootReducer.tsx
+++ src/app/store/rootReducer.tsx
@@ -1,36 +1,17 @@
 
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counter.slice';
-import cakeSlice from '../features/cake/cake.slice';
-import userSlice from '../features/user/user.slice';
+// import counterReducer from '../features/counter/counter.slice';
+// import cakeSlice from '../features/cake/cake.slice';
+// import userSlice from '../features/user/user.slice';
 
-
-function parentPath(url: string) {
-  return url.split('/').slice(-2).join('/');
-}
-
-//async function smartReducers() {
-
-// let reducers: { [k: string]: any; } = {};
-const context = require.context('./../features/', true, /\.slice\.tsx$/);
+const context = require.context('./../features/', true, /\.slice\.tsx$/i);
 const reducers = context.keys().reduce((acc: any, key) => {
-  const reducerName = key.replace(/\.\/.js/g, '');
+  const reducerNamePattern = /([^\/]+)$/;
+  const reducerName = key.match(reducerNamePattern)?.toString().split('.')[0] as string;
   acc[reducerName] = context(key).default;
   return acc;
 }, {});
-//const modulePaths = context.keys();
-// modulePaths.forEach(async (modulePath) => {
-//   const a = modulePath.slice(1);
-//   const b = `${'./../features'}${modulePath.slice(1)}`;
-//   const loadLibrary: any = async () => await import(`${'./../features'}${modulePath.slice(1)}`);
-//   console.log(loadLibrary);
-//   const reducerName = parentPath(modulePath);
-//   reducers[reducerName] = loadLibrary.default;
-// });
-//   return reducers;
-// }
 
-// const reducers: any = smartReducers();
 export const store = configureStore({
   reducer: combineReducers(reducers)
   // reducer: {
